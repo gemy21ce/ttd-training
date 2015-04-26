0e65a9c33a88cb0ec7e3dc4caea467c998bc5f53
Gamal_sabdelmeged <Gamal_sabdelmeged@EF-BS-00204.EFinance.com.eg>
2015-04-26 12:51:50 +0300

    formatting imports

--- src/main/java/com/dm/agility/ttdtraining/controller/FeedReader.java
+++ src/main/java/com/dm/agility/ttdtraining/controller/FeedReader.java
@@ -7,7 +7,6 @@ package com.dm.agility.ttdtraining.controller;
 
 import com.dm.agility.ttdtraining.service.RSSReader;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/test/java/com/dm/agility/ttdtraining/service/ServiceReaderTest.java
+++ src/test/java/com/dm/agility/ttdtraining/service/ServiceReaderTest.java
@@ -10,7 +10,6 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
